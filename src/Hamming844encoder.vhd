dde9663b06ffff68929e4b54bd62c66d0e58bdf8
Jit <kanetkarster@gmail.com>
2015-12-12 15:53:23 -0500

    tested hamming decoder and bit converter

--- src/Hamming844encoder.vhd
+++ src/Hamming844encoder.vhd
@@ -1,6 +1,6 @@
 --! @file Hamming844Encoder.vhd
+--! @author Jit Kanetkar (2015)
 --! @brief Encodes a 4 bit input using the Hamming 844 formula
-
 
 --! include standard library
 library ieee;
@@ -13,11 +13,12 @@ use ieee.std_logic_1164.all;
 --! @param data data to be encoded. (m0 = data(0), ... m3 = data(3))
 --! @retval encoded_data data encoded with the Hamming formula
 --! 
---! encoded_data (0->3) = data(0->3)
---! encoded_data (4) = data(0) + data(1) + data(2)
---! encoded_data (5) = data(0) + data(1) + data(3)
---! encoded_data (6) = data(0) + data(2) + data(3)
---! encoded_data (7) = data(1) + data(2) + data(3)
+--! implemeted as follows:
+--! encoded_data (7->4) = data(3->0)
+--! encoded_data (3) = data(0) + data(1) + data(2)
+--! encoded_data (2) = data(0) + data(1) + data(3)
+--! encoded_data (1) = data(0) + data(2) + data(3)
+--! encoded_data (0) = data(1) + data(2) + data(3)
 entity Hamming844Encoder is
 	port (
 		data : in std_logic_vector(3 downto 0);
@@ -30,12 +31,12 @@ architecture impl of Hamming844Encoder is
 
 begin
 	--! assign top 4 bits to be same as input bits
-	encoded_data(3 downto 0) <= data;
+	encoded_data(7 downto 4) <= data;
 	
 	--! assign parity bits
-	encoded_data(4) <= data(0) XOR data(1) XOR data(2);
-	encoded_data(5) <= data(0) XOR data(1) XOR data(3);
-	encoded_data(6) <= data(0) XOR data(2) XOR data(3);
-	encoded_data(7) <= data(1) XOR data(2) XOR data(3);
+	encoded_data(3) <= data(3) XOR data(2) XOR data(1);
+	encoded_data(2) <= data(3) XOR data(2) XOR data(0);
+	encoded_data(1) <= data(3) XOR data(1) XOR data(0);
+	encoded_data(0) <= data(2) XOR data(1) XOR data(0);
 
 end impl;
